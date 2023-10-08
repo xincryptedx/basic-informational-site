70e5d58fd1f256f359b68daa2f3fc41aa9790fba
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-10-08 17:06:41 -0400

    Add basic server usting http and fs modules

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs/promises");
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  let filePath = "./index.html";
+
+  switch (url) {
+    case "/":
+      filePath = "./index.html";
+      break;
+    case "/about":
+      filePath = "./pages/about.html";
+      break;
+    case "/contact-me":
+      filePath = "./pages/contact-me.html";
+      break;
+    default:
+      filePath = "./pages/404.html";
+      break;
+  }
+
+  const asyncReadFile = async (res, filePath) => {
+    try {
+      const data = await fs.readFile(filePath, "utf8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    } catch (err) {
+      console.log(err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+  };
+
+  asyncReadFile(res, filePath);
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

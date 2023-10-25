e79b678913a73bace4868a2c170402cbf93a75d1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-10-24 20:06:30 -0400

    Add routes for main pages

--- src/server.js
+++ src/server.js
@@ -1,58 +1,20 @@
-/* const http = require("http");
-const fs = require("fs/promises");
-
-const hostname = "127.0.0.1";
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  let filePath = "./index.html";
-
-  switch (url) {
-    case "/":
-      filePath = "./src/index.html";
-      break;
-    case "/about":
-      filePath = "./src/pages/about.html";
-      break;
-    case "/contact-me":
-      filePath = "./src/pages/contact-me.html";
-      break;
-    default:
-      filePath = "./src/pages/404.html";
-      break;
-  }
-
-  const asyncReadFile = async (res, filePath) => {
-    try {
-      const data = await fs.readFile(filePath, "utf8");
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    } catch (err) {
-      console.log(err);
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
-    }
-  };
-
-  asyncReadFile(res, filePath);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
- */
-
 const express = require("express");
 const app = express();
 const path = require("path");
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "contact-me.html"));
+});
+
 const port = 8080;
-
-// Middleware that serves static files
-app.use(express.static(path.join(__dirname, "public", "html")));
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
